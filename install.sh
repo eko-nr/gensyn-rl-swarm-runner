6aecbe9d48f2327d862ea81c91b017a8d1bd9e6a
eko-nr <eko.nr@yahoo.com>
2025-07-11 21:23:27 +0700

    chore: update packages

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ chmod +x ./start.sh ./run_unless_stop.sh
 
 mkdir -p $ROOT && cd $ROOT
 
+sudo apt update -y
 apt install screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
 apt install python3 python3-pip python3-venv python3-dev -y
 
