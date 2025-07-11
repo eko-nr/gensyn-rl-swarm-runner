f5a2ae4535ebd8e11878979213f2ffda7d145783
eko-nr <eko.nr@yahoo.com>
2025-07-11 21:49:46 +0700

    chore: installing nodejs

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ sudo apt update -y
 apt install screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
 apt install python3 python3-pip python3-venv python3-dev -y
 
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+apt install -y nodejs
+node -v
+npm install -g yarn
+yarn -v
+
 git clone https://github.com/gensyn-ai/rl-swarm
 
 
