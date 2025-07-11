a40d9596b5c9298120bdb60b77c5a7b066338115
eko-nr <eko.nr@yahoo.com>
2025-07-11 19:04:42 +0700

    chore: install python and some deps

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ chmod +x ./start.sh ./run_unless_stop.sh
 
 mkdir -p $ROOT && cd $ROOT
 
+apt install screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
+apt install python3 python3-pip python3-venv python3-dev -y
+
 git clone https://github.com/gensyn-ai/rl-swarm
 
 
