f92be50a18aecf710654c2a88ee3bd812282ac42
eko-nr <eko.nr@yahoo.com>
2025-07-12 01:12:27 +0700

    chore: if nodejs installed dont install again

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ROOT="$HOME"/app
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-chmod +x ./start.sh ./run_unless_stop.sh ./start_max_ram_8.5GB.sh
+sudo chmod +x ./start.sh ./run_unless_stop.sh ./start_max_ram_8.5GB.sh
 
 mkdir -p $ROOT && cd $ROOT
 
@@ -9,11 +9,17 @@ sudo apt update -y
 apt install screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
 apt install python3 python3-pip python3-venv python3-dev -y
 
-curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
-apt install -y nodejs
-node -v
-npm install -g yarn
-yarn -v
+# Check if Node.js exists
+if command -v node &> /dev/null; then
+    echo "Node.js is already installed: $(node --version)"
+else
+    echo "Node.js not found. Installing..."
+    curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+    apt install -y nodejs
+    node -v
+    npm install -g yarn
+    yarn -v
+fi
 
 git clone https://github.com/gensyn-ai/rl-swarm
 
