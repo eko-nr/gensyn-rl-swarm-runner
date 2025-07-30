0c0dbca2d1356154be26da9fa04079a1447c5740
eko-nr <eko.nr@yahoo.com>
2025-07-30 22:51:19 +0700

    chore: add install package python

--- install.sh
+++ install.sh
@@ -29,12 +29,6 @@ else
     git clone https://github.com/gensyn-ai/rl-swarm
 fi
 
-python3 -m venv .venv
-
-source .venv/bin/activate
-# if not worked, then:
-. .venv/bin/activate
-
 echo ">> Getting requirements..."
 pip install --upgrade pip
 
