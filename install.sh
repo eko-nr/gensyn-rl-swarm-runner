408e19bb91f6fc546cbffa262ec8c4c905651fc7
eko-nr <eko.nr@yahoo.com>
2025-07-31 11:22:09 +0700

    chore: fix path install deps

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ else
     git clone https://github.com/gensyn-ai/rl-swarm
 fi
 
+cd $RL_SWARM
+
 python3 -m venv .venv
 
 source .venv/bin/activate
@@ -43,3 +45,5 @@ pip install gensyn-genrl==0.1.4
 pip install reasoning-gym>=0.1.20 # for reasoning gym env
 pip install trl # for grpo config, will be deprecated soon
 pip install hivemind@git+https://github.com/gensyn-ai/hivemind@639c964a8019de63135a2594663b5bec8e5356dd # We need the latest, 1.1.11 is broken
+
+cd $SCRIPT_DIR
